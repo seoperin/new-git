f5d43ae374b121f95febe79678eb6fc430dcebd3
seoperin <seoperin@gmail.com>
2016-03-03 11:52:01 +0300

    add files

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  database : 'express',
+  user     : 'root',
+  password : ''
+});
+connection.connect();
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+/*app.get('/', function (req, res) {
+  res.send('Hello express');
+});*/
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'Hello express',
+    test: connection.query('SELECT * FROM users'),
+    items: [
+      { id:1, desc: 'ololo'},
+      { id:2, desc: 'atata'},
+      { id:3, desc: 'omnomnomn'}
+    ]
+  });
+});
+
+app.listen(8080, function () {
+  console.log('ready');
+});
